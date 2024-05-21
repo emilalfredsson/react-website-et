539d7c604609bf5f47029b47a64a2b738ddef8f6
unknown <ema22@hi.is>
2024-05-21 23:57:25 +0000

    logos

--- react-website-et/src/components/Cards.js
+++ react-website-et/src/components/Cards.js
@@ -1,15 +1,57 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from 'react';
+import CardItem from './CardItem';
 import './Cards.css';
 
 function Cards() {
+  // Example logo data
+  const logos = [
+    { src: 'images/et-images/customers/orkan.png'},
+    { src: 'images/et-images/customers/lyfjaval.jpg'},
+    { src: 'images/et-images/customers/logoflex.png'},
+    { src: 'images/et-images/customers/Landsbankinn.png'},
+    { src: 'images/et-images/customers/Skeljungur_logo.jpg'},
+    { src: 'images/et-images/customers/ksi.png'},
+    { src: 'images/et-images/customers/hopp.jpg'},
+    { src: 'images/et-images/customers/extra.jpg'},
+    { src: 'images/et-images/customers/vinbudin.jpg'},
+    { src: 'images/et-images/customers/Snaefellsbaer.jpg'},
+    { src: 'images/et-images/customers/logreglan.jpg'},
+    { src: 'images/et-images/customers/Pósturinn.jpg'},
+    { src: 'images/et-images/customers/ua.png'},
+    { src: 'images/et-images/customers/Akureyri.jpg'},
+    { src: 'images/et-images/customers/logo_samherji.jpg'},
+    { src: 'images/et-images/customers/icelandair.svg'},
+    { src: 'images/et-images/customers/holdur.jpg'},
+    { src: 'images/et-images/customers/kristjans.png'},
+    { src: 'images/et-images/customers/heimstaden.jpg'},
+    { src: 'images/et-images/customers/bmvalla.png'},
+    { src: 'images/et-images/customers/felagsbustadir.jpg'},
+    { src: 'images/et-images/customers/flugger.jpg' },
+    { src: 'images/et-images/customers/greifinn.png'},
+    { src: 'images/et-images/customers/taekni.png'}         
+  ];
+
   return (
     <div className='cards'>
         <div className='cards__box'>
-            <h1 className='cards__header'>Hjá E Tómassyni sérhæfum við okkur í bílastæða- og vegmerkingum fyrir fyrirtæki og einstaklinga</h1> 
-            <h2 className='cards__text'>E Tómasson er rótgróið fjölskyldu fyrirtæki sem hefur verið starfandi í yfir 20 ár. Fyrirtækið býr yfir fullkomnum vélakosti og þrautreyndum starfsmönnum sem tryggir viðskiptavinum fyrirtækisins ávallt fullkomin gæði.</h2>
+            <h1 className='cards__header'>
+              E Tómasson er rótgróið fjölskyldu fyrirtæki sem hefur verið starfandi í yfir 20 ár. <br/> <br/> 
+              Fyrirtækið býr yfir fullkomnum vélakosti og þrautreyndum starfsmönnum sem tryggir viðskiptavinum fyrirtækisins ávallt fullkomin gæði.
+            </h1> 
         </div>
         <div className="cards__container">
+            <div className='cards__text'>
+                <h2>Meðal ánægðara viðskiptavina fyrirtækisins</h2>
+            </div>
+            <div className='cards__logo-section'>
+            <div className='cards__logo-grid'>
+              {logos.map((logo, index) => (
+                <a key={index} href={logo.url} target="_blank" rel="noopener noreferrer" className='cards__logo-item'>
+                  <img src={logo.src} alt={`Logo ${index + 1}`} />
+                </a>
+              ))}
+            </div>
+            </div>   
             <div className="cards__wrapper">
                 <ul className="cards__items">
                     <CardItem 
@@ -26,9 +68,9 @@ function Cards() {
                     />
                 </ul>
             </div>
-        </div>      
+        </div>   
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
--- react-website-et/src/components/HeroSection.js
+++ react-website-et/src/components/HeroSection.js
@@ -11,10 +11,10 @@ const images = [
 ];
 
 const textContent = [
-  { heading: 'Bílastæða- og vegmerkingar', subheading: 'Hvort sem um er að ræða bílastæði hjá innkeyrslunni eða heilt bæjarfélag, þá búum við yfir fullkomnasta tækjakosti sem fáanlegur er til að skila af okkur fullkomnu verki. Málningin sem við notumst við er samþykkt af Vegagerðinni, og er víðast hvar notuð í Evrópu.' },
-  { heading: 'Stöðvunar Dregill', subheading: 'Byltingarkennd lausn sem eykur umferðaröryggi verulega. Það styttir stöðvunarvegalengd ökutækis um allt að 30% við erfiðar veðuraðstæður. Hæfni þess til að aðlaga sig að mismunandi gerðum landslags gerir Dregilinn áreiðanlegan, sérstaklega í kringum gangbrautir. Það er því fjárfesting í öruggum vegum framtíðarinna.' },
-  { heading: 'Gangbrautarkerfi', subheading: 'Þökk sé hreyfiskynjurum, stillir tækið sjálfkrafa birtustig lýsingarinnar á ljósastaurum, það kviknar aðeins af fullum krafti þegar gangandi vegfarandi er við gangbraut. Bæði eykur það sýnileika og sparar orku kostnað um leið. Hægt er að stækka þessa lausn með samstilltum led ljósum sem beina í átt að ökumanni, einnig hægt að bæta við hljóðum, eftirliti osfrv.' },
-  { heading: 'Vegleiðari', subheading: 'Snjómokstursgerður vegleiðari í steypujárnshlíf sem tryggir ekki aðeins framúrskarandi sýnileika á veginum, heldur er hann líka einstaklega endingargóður og veðurheldur. Skiptanleg endurskinsmerki og lögun þess tryggja auðvelt viðhald og aukið viðnám gegn skemmdum, sem skilar sér í langtímavörn endurskinsmerkisins gegn skemmdum af völdum ökutækja.' }
+  { heading: 'Bílastæða- og vegmerkingar'},
+  { heading: 'Stöðvunar Dreglar'},
+  { heading: 'Gangbrautarkerfi'},
+  { heading: 'Vegleiðarar'}
 ];
 
 function HeroSection() {
@@ -28,7 +28,7 @@ function HeroSection() {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
         setFadeText(true); // Fade in the text after changing the image
       }, 500); // Wait for the text to fade out before changing the image
-    }, 10000); // Change image and text every 10 seconds
+    }, 5000); // Change image and text every 5 seconds
 
     return () => clearInterval(interval); // Clean up the interval on component unmount
   }, []);
