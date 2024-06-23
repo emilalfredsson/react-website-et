3953b1a65a66e597f63f093d66da594c62d2caab
Emil Alfreðsson <emilalfredsson@hotmail.com>
2024-06-23 21:12:56 +0000

    bug fix

--- react-website-et/src/App.js
+++ react-website-et/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Main from './components/pages/Main';
 import Contact from './components/pages/Contact';
 import Safety from './components/pages/Safety';
-import Services from './components/pages/Services';
 import Vegmerkingar from './components/pages/Vegmerkingar';
 import Vegleidarar from './components/pages/Vegleidarar';
 import Stodvunardreglar from './components/pages/Stodvunardreglar';
@@ -20,7 +19,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact Component= {Main}/>
-          <Route path='/services' exact Component= {Services}/>
           <Route path='/safety' exact Component= {Safety}/>
           <Route path='/contact' exact Component= {Contact}/>
           <Route path='/vegmerkingar' exact Component= {Vegmerkingar}/>
