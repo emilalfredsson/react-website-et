616b3ff0534be3b8114ffeec02669bf6d04f2af0
Emil Alfreðsson <emilalfredsson@hotmail.com>
2024-06-28 17:59:38 +0000

    routing tweaks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Main from './components/pages/Main';
 import Contact from './components/pages/Contact';
